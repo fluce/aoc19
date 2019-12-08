725ab7a85d23189a86b23fa2b23823f6c965f88e
Frédéric Luce <frederic.luce@gmail.com>
2019-12-08 01:14:14 +0100

    Day 6 done

--- Day6.Tests/Day6Test.cs
+++ Day6.Tests/Day6Test.cs
@@ -1,20 +1,94 @@
 using System;
+using System.Linq;
+using System.Collections.Generic;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace Day6.Tests
 {
     public class Day6Test
     {
+        readonly ITestOutputHelper console;
+
+        public Day6Test(ITestOutputHelper console)
+        {
+            this.console = console;
+        }
+
         [Theory]
         [InlineData("COM)A","COM","A")]
         [InlineData("A)B","A","B")]
-        public void ParseLine(string input,string p1, string p2)
+        public void TestParseLine(string input,string p1, string p2)
         {
             var node=InputParser.ParseOne(input);
             Assert.Equal(p2,node.Name);
-            Assert.NotNull(node.ParentNode);
-            Assert.Equal(p1,node.ParentNode.Name);
-            Assert.Null(node.ParentNode.ParentNode);
+            Assert.NotNull(node.OrbitsAround);
+            Assert.Equal(p1,node.OrbitsAround.Name);
+            Assert.Null(node.OrbitsAround.OrbitsAround);
+            Assert.Single(node.OrbitsAround.Satellites);
+            Assert.Same(node,node.OrbitsAround.Satellites.Single());
+            Assert.Empty(node.Satellites);            
         }
+
+        [Theory]
+        [InlineData(@"COM)B,B)C,C)D,D)E,E)F,B)G,G)H,D)I,E)J,J)K,K)L")]
+        [InlineData(@"COM)B,C)D,B)C")]
+        public void TestParser(string input)
+        {
+            var parser=new InputParser(input.Split(","));
+            parser.Parse();
+            Assert.Single(parser.Roots);
+            InputParser.Dump(parser.Roots.First(),console.WriteLine);
+        }
+
+        [Fact]
+        public void TestCalculateOrbits()
+        {
+            string input=@"COM)B,B)C,C)D,D)E,E)F,B)G,G)H,D)I,E)J,J)K,K)L";
+            var parser=new InputParser(input.Split(","));
+            parser.Parse();
+            Assert.Single(parser.Roots);
+            InputParser.Dump(parser.Roots.First(),console.WriteLine);
+
+            Assert.Equal(3,InputParser.CalculateDirectAndIndirectOrbits(parser.AllObjects["D"]));
+            Assert.Equal(7,InputParser.CalculateDirectAndIndirectOrbits(parser.AllObjects["L"]));
+            Assert.Equal(0,InputParser.CalculateDirectAndIndirectOrbits(parser.AllObjects["COM"]));
+
+            var result=parser.AllObjects.Values.Sum(InputParser.CalculateDirectAndIndirectOrbits);
+            Assert.Equal(42,result);
+        }
+
+        [Theory]
+        [InlineData(@"COM)B,B)C,C)D,D)E,E)F,B)G,G)H,D)I,E)J,J)K,K)L", "C", "B,COM")]
+        [InlineData(@"COM)B,B)C,C)D,D)E,E)F,B)G,G)H,D)I,E)J,J)K,K)L", "L", "K,J,E,D,C,B,COM")]
+        [InlineData(@"COM)B,B)C,C)D,D)E,E)F,B)G,G)H,D)I,E)J,J)K,K)L", "H", "G,B,COM")]
+        public void TestGetPathToRoot(string input, string from, string expectedResult)
+        {
+            var parser=new InputParser(input.Split(","));
+            parser.Parse();
+            InputParser.Dump(parser.Roots.First(),console.WriteLine);
+            var r=string.Join(",",InputParser.GetPathToRootSpatialObject(parser.AllObjects[from]).Select(x=>x.Name));
+            Assert.Equal(expectedResult,r);
+        }
+
+        [Theory]
+        [InlineData(@"COM)B,B)C,C)D,D)E,E)F,B)G,G)H,D)I,E)J,J)K,K)L", "B", "COM", 1)]
+        [InlineData(@"COM)B,B)C,C)D,D)E,E)F,B)G,G)H,D)I,E)J,J)K,K)L", "C", "COM", 2)]
+        [InlineData(@"COM)B,B)C,C)D,D)E,E)F,B)G,G)H,D)I,E)J,J)K,K)L", "C", "G", 0)]
+        [InlineData(@"COM)B,B)C,C)D,D)E,E)F,B)G,G)H,D)I,E)J,J)K,K)L,K)YOU,I)SAN", "YOU", "SAN", 4)]
+        public void TestCalcTransferCount(string input, string from, string to, int expectedResult)
+        {
+            var parser=new InputParser(input.Split(","));
+            parser.Parse();
+            InputParser.Dump(parser.Roots.First(),console.WriteLine);
+
+            console.WriteLine(string.Join(",",InputParser.GetPathToRootSpatialObject(parser.AllObjects[from]).Select(x=>x.Name)));
+            console.WriteLine(string.Join(",",InputParser.GetPathToRootSpatialObject(parser.AllObjects[to]).Select(x=>x.Name)));
+
+            var (degenerative,n)=parser.CalculateOrbitalTransfersCount(parser.AllObjects[from],parser.AllObjects[to], console.WriteLine);
+            if (degenerative) console.WriteLine("Degenerative case");
+            Assert.Equal(expectedResult,n);
+        }
+
     }
 }
--- Day6/Program.cs
+++ Day6/Program.cs
@@ -1,31 +1,139 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Day6
 {
-    public class Node
+    public class SpatialObject
     {
-        public Node ParentNode {get;set;}
+        public SpatialObject OrbitsAround {get;set;}
         public string Name {get;set;}
+        public ICollection<SpatialObject> Satellites {get;} = new List<SpatialObject>();
+        public int Color {get;set;}
     }
     public class InputParser
     {        
-        public InputParser(string input)
+        public InputParser(params string[] input)
         {
+            Input=input;
+        }
+        public string[] Input {get;private set;}
+
+        public IEnumerable<SpatialObject> Roots {get;private set;}
+        public IDictionary<string,SpatialObject> AllObjects {get;private set;}
+        public void Parse()
+        {                
+            Dictionary<string,SpatialObject> objectIndex=new Dictionary<string, SpatialObject>();
+
+            foreach(var iterSpatialObject in Input.Select(ParseOne))
+            {
+                var spatialObject=iterSpatialObject;
+                if (objectIndex.TryGetValue(spatialObject.Name, out var existingSpatialObject)) 
+                {
+                    //throw new InvalidOperationException($"SpatialObject {spatialObject.Name} already exists");
+                    if (existingSpatialObject.OrbitsAround!=null)
+                        throw new InvalidOperationException($"SpatialObject {spatialObject.Name} already has a parent : {existingSpatialObject.OrbitsAround.Name}");
+                    existingSpatialObject.OrbitsAround=spatialObject.OrbitsAround;
+                    existingSpatialObject.OrbitsAround.Satellites.Clear();
+                    existingSpatialObject.OrbitsAround.Satellites.Add(existingSpatialObject);
+                    spatialObject=existingSpatialObject;
+                } else {
+                    objectIndex.Add(spatialObject.Name,spatialObject);
+                }
+
+                if (objectIndex.TryGetValue(spatialObject.OrbitsAround.Name, out var orbitsAround))  // parent found
+                {
+                    spatialObject.OrbitsAround=orbitsAround;
+                    orbitsAround.Satellites.Add(spatialObject);
+                } else { // parent not found
+                    objectIndex.Add(spatialObject.OrbitsAround.Name,spatialObject.OrbitsAround);
+                }
+
+            }
+            Roots=objectIndex.Values.Where(x=>x.OrbitsAround==null).ToList();
+            AllObjects=objectIndex;
 
         }
 
-        public static Node ParseOne(string input)
+        public static SpatialObject ParseOne(string input)
         {
             var r=input.Split(')');
-            return new Node {Name=r[1], ParentNode=new Node{Name=r[0]}};
+            var n=new SpatialObject {Name=r[1], OrbitsAround=new SpatialObject{Name=r[0]}};
+            n.OrbitsAround.Satellites.Add(n);
+            return n;
         }
+
+        public static int CalculateDirectAndIndirectOrbits(SpatialObject obj)
+        {
+            int n=0;
+            while (obj.OrbitsAround!=null) { n++; obj=obj.OrbitsAround; }
+            return n;
+        }
+
+        public static void Dump(SpatialObject obj, Action<string> write, string level="")
+        {
+            write($"{level}{obj.Name} - {obj.Color}");
+            foreach(var s in obj.Satellites)
+                Dump(s,write,level+"|  ");
+        }
+
+        public static IEnumerable<SpatialObject> GetPathToRootSpatialObject(SpatialObject obj)        
+        {
+            obj=obj.OrbitsAround;
+            while (obj!=null){
+                yield return obj;
+                obj=obj.OrbitsAround;
+            }
+        }
+
+        public (bool,int) CalculateOrbitalTransfersCount(SpatialObject from, SpatialObject to, Action<string> write)
+        {
+            foreach(var o in AllObjects.Values) o.Color=0;
+
+            var fromPath=GetPathToRootSpatialObject(from);
+            var root=fromPath.Last();
+            foreach(var o in fromPath) o.Color=1;
+
+            Dump(root,write);
+
+            var toPath=GetPathToRootSpatialObject(to);
+            var tillIntersection1=toPath.TakeWhile(x=>x.Color==0);
+            if (tillIntersection1.Count()==0) // Cas dégénéré, to déjà sur le chemin vers la racine depuis from
+            {
+                write("Cas dégénéré");
+                var tillIntersection2=fromPath.TakeWhile(x=>x!=toPath.FirstOrDefault());
+                foreach(var o in tillIntersection2) o.Color=3;
+                Dump(root,write);
+                return (true,tillIntersection2.Count());
+            } else {
+                var intersection=tillIntersection1.Last().OrbitsAround.Color=2;
+                Dump(root,write);
+                var tillIntersection2=fromPath.TakeWhile(x=>x.Color!=2);
+                foreach(var o in tillIntersection2) o.Color=4;
+                Dump(root,write);
+                return (false,tillIntersection1.Count()+tillIntersection2.Count());
+            }
+        }
+
     }
 
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var parser=new InputParser(System.IO.File.ReadAllLines(args[0]));
+            parser.Parse();
+            foreach(var r in parser.Roots) InputParser.Dump(r,Console.WriteLine);
+
+            var result=parser.AllObjects.Values.Sum(InputParser.CalculateDirectAndIndirectOrbits);
+
+            var (degenerative,n)=parser.CalculateOrbitalTransfersCount(parser.AllObjects["YOU"],parser.AllObjects["SAN"], Console.WriteLine);
+            if (degenerative) Console.WriteLine("Degenerative case");
+
+            Console.WriteLine($"Total orbits : {result}");
+            Console.WriteLine($"Total transfers : {n}");
+            
+
         }
     }
 }
